890a5e06a42c1bee0a39b01285cc084fa3775406
Danila Fomin <dangdancheg@gmail.com>
2021-12-16 19:05:38 +0300

    moved theme to project

---
+++ lib/theme/export.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+var appTheme = ThemeData(
+  pageTransitionsTheme: const PageTransitionsTheme(
+    builders: {
+      TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder(),
+      TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+    },
+  ),
+  canvasColor: Colors.transparent,
+  backgroundColor: const Color.fromRGBO(42, 42, 114, 1.0), // purple
+  focusColor: const Color.fromRGBO(234, 246, 255, 1.0), // white
+  hintColor: const Color.fromRGBO(255, 164, 0, 1.0), // yellow
+  hoverColor: const Color.fromRGBO(0, 159, 253, 1.0), // blue
+  primaryColorDark: const Color.fromRGBO(35, 37, 40, 1.0), // black
+  textTheme: const TextTheme(
+    headline1: TextStyle(
+      color: Color.fromRGBO(234, 246, 255, 1.0),
+      fontSize: 36,
+      fontFamily: 'Hind',
+    ),
+    headline2: TextStyle(
+      color: Color.fromRGBO(234, 246, 255, 1.0),
+      fontSize: 24.2,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w200,
+    ),
+    headline3: TextStyle(
+      color: Color.fromRGBO(35, 37, 40, 1.0),
+      fontSize: 44,
+      fontFamily: 'Hind',
+    ),
+    headline4: TextStyle(
+      color: Color.fromRGBO(35, 37, 40, 1.0),
+      fontSize: 26.2,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w200,
+    ),
+    headline5: TextStyle(
+      color: Color.fromRGBO(35, 37, 40, 1.0),
+      fontSize: 16.2,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w500,
+    ),
+    headline6: TextStyle(
+      color: Color.fromRGBO(35, 37, 40, 1.0),
+      fontSize: 14.2,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w400,
+    ),
+    overline: TextStyle(
+      color: Color.fromRGBO(234, 246, 255, 1.0),
+      fontSize: 15.4,
+    ),
+    button: TextStyle(
+      color: Color.fromRGBO(234, 246, 255, 1.0),
+      fontSize: 21.2,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w400,
+    ),
+    subtitle2: TextStyle(
+      color: Color.fromRGBO(234, 246, 255, 1.0),
+      fontSize: 16.2,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w300,
+    ),
+    bodyText1: TextStyle(
+      color: Color.fromRGBO(234, 246, 255, 1.0),
+      fontSize: 19.6,
+      fontFamily: 'Hind',
+      fontWeight: FontWeight.w300,
+    ),
+  ),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      primary: const Color.fromRGBO(234, 246, 255, 1.0),
+      side: const BorderSide(
+        color: Color.fromRGBO(234, 246, 255, 1.0),
+        width: 0.58,
+      ),
+      backgroundColor: const Color.fromRGBO(42, 42, 114, 1.0),
+      textStyle: const TextStyle(
+        fontSize: 24.4,
+        fontFamily: 'Hind',
+        fontWeight: FontWeight.w300,
+      ),
+    ),
+  ),
+);
