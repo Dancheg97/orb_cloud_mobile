2d592ae3969652d9af2d246276fb6db0e524726e
Danila Fomin <dangdancheg@gmail.com>
2021-12-17 14:11:16 +0300

    commented some color themes, todo remove them

--- lib/main.dart
+++ lib/main.dart
@@ -12,9 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: appTheme,
       home: Scaffold(
         body: Center(
           child: Text(lang.pageMarket),
