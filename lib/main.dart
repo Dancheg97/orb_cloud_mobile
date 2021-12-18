32443ce256e3706c827347aa2d8bc613f5b015fe
Danila Fomin <dangdancheg@gmail.com>
2021-12-18 16:15:54 +0300

    added subscription methods to api calls, not to make unneeded refreshes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
-import 'package:ORBmobile/intro/intro.dart';
-import 'package:ORBmobile/language/language.dart';
 import 'package:flutter/material.dart';
 import 'theme/theme.dart';
 
 void main() {
-  runApp(App());
+  runApp(const App());
 }
 
 class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        theme: appTheme,
-        home: Center(),
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: appTheme,
+      home: const Center(
+        child: Text('sss'),
+      ),
+    );
+  }
 }
