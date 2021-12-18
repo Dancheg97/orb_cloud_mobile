527c79138685f0a2e150751f0e660894b3334b42
Danila Fomin <dangdancheg@gmail.com>
2021-12-18 18:04:59 +0300

    added hsa trades method to requests

--- lib/api/main.dart
+++ lib/api/main.dart
@@ -2,3 +2,34 @@ export 'api.pb.dart';
 export 'api.pbenum.dart';
 export 'api.pbgrpc.dart';
 export 'api.pbjson.dart';
+
+import 'api.pbgrpc.dart';
+import 'package:grpc/grpc.dart';
+
+final channel = ClientChannel(
+  '46.138.246.106',
+  port: 8080,
+  options: ChannelOptions(
+    credentials: const ChannelCredentials.insecure(),
+    codecRegistry: CodecRegistry(
+      codecs: const [
+        GzipCodec(),
+        IdentityCodec(),
+      ],
+    ),
+  ),
+);
+
+final infoStub = InfoClient(
+  channel,
+  options: CallOptions(
+    timeout: const Duration(seconds: 1),
+  ),
+);
+
+final userStub = UserClient(
+  channel,
+  options: CallOptions(
+    timeout: const Duration(seconds: 1),
+  ),
+);
