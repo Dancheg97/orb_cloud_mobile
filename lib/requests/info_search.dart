a408cfe74e6fa0d9b70d36e42b10d9b5c76be857
Danila Fomin <dangdancheg@gmail.com>
2021-12-19 18:24:40 +0300

    .

--- lib/requests/info_search.dart
+++ lib/requests/info_search.dart
@@ -1,4 +1,9 @@
 import '../api/main.dart';
 import 'dart:typed_data';
-import 'info_user.dart';
 
+Future<List<Uint8List>> infoSearch(String input) async {
+  var response = await infoStub.search(
+    InfIn_Text(text: input),
+  );
+  return response.marketAdresses.map((e) => Uint8List.fromList(e)).toList();
+}
