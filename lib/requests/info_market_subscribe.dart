1cd68cdbd252f82cfa6f3b3dc760682222b31c8e
DANCHE <dangdancheg@gmail.com>
2021-12-19 16:21:56 +0300

    rfc requests

--- lib/requests/info_market_subscribe.dart
+++ lib/requests/info_market_subscribe.dart
@@ -1,3 +1,7 @@
+import 'dart:typed_data';
+
+import 'package:ORBmobile/api/main.dart';
+
 import 'info_market.dart';
 
 class MarketInfoStream {
@@ -7,4 +11,18 @@ class MarketInfoStream {
     required this.stream,
     required this.cancel,
   });
+}
+
+MarketInfoStream marketSubscribe(Uint8List adress) {
+  var grpcStream = infoStub.marketSubscribe(
+    InfIn_Adress(adress: adress),
+  );
+  var dartStream = grpcStream.map(
+    (response) => transformMarketInfo(response),
+  );
+  var userInfoStream = MarketInfoStream(
+    stream: dartStream,
+    cancel: grpcStream.cancel,
+  );
+  return userInfoStream;
 }
