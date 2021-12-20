b0a82968b1acf598119b32f48aacb22ae5e9c657
Danila Fomin <dangdancheg@gmail.com>
2021-12-20 11:43:51 +0300

    optimized user send message a bit

--- lib/requests/user_send.dart
+++ lib/requests/user_send.dart
@@ -6,15 +6,15 @@ import 'package:fixnum/fixnum.dart';
 
 Future<bool> userSend(Uint8List adress, int amount) async {
   var pubBytes = await Crypter.peronalKeyBytes();
-  var amountBytes = Int64(amount).toBytes();
+  var amountInt64 = Int64(amount);
   var response = await userStub.send(
     UserRequests_Send(
       publicKey: pubBytes,
-      sendAmount: Int64(amount),
+      sendAmount: amountInt64,
       recieverAdress: adress,
       sign: await Crypter.sign(
         Uint8List.fromList(
-          pubBytes + amountBytes + adress,
+          pubBytes + amountInt64.toBytes() + adress,
         ),
       ),
     ),
